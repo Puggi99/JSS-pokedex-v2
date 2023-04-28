b85c77f3dc7418861c9be469d161565414d67a46
Puggi99 <127298032+Puggi99@users.noreply.github.com>
2023-04-28 13:03:29 +0200

    ddd

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let actualPageIndex
-
+// SCARICA ESTENSIONE CORS SU FIREFOX
 getNextPage();
 
 function getNextPage() {
@@ -8,14 +8,51 @@ function getNextPage() {
     } else {
         actualPageIndex++;
     }
-    PokeService.getPage(actualPageIndex).then(page =>{
-        console.log(page)
+    PokeService.getPage(actualPageIndex).then(pokemons => {
+       
+        displayPokemons(pokemons)
     })
 }
 
 function getPreviousPage() {
     actualPageIndex--;
-    PokeService.getPage(actualPageIndex).then(page =>{
-        console.log(page);
+    PokeService.getPage(actualPageIndex).then(pokemons => {
+        displayPokemons(pokemons)
     })
+}
+
+function displayPokemons(pokemons) {
+    const pokemonContainer = document.getElementById('pokemon-container');
+    pokemonContainer.innerHTML = '';
+    for (const pokemon of pokemons) {
+        console.log(pokemons)
+        
+        pokemonContainer.innerHTML += `
+        <details>
+            <summary>
+                <span>${pokemon.id}</span>
+                <img class ="list-image" src="${pokemon.sprites.front_default}" alt="">
+                <span>${pokemon.name}</span>
+                <div class="spacer"></div>
+                ${pokemon.types.map(obj => `<span class="type">${obj.type.name}</span> `).join('/')}
+            </summary>
+            <div>
+                <ul>
+                    ${createAbilitiesList(pokemon)}
+                </ul>
+            </div>
+        </details>
+        `
+    }
+}
+
+
+function createAbilitiesList(pokemon){
+
+    let abilitiesHtml = '';
+        
+    for (const object of pokemon.abilities) {
+        abilitiesHtml += `<li>${object.ability.name}</li>`
+    }return abilitiesHtml;
+
 }
--- services/poke-services.js
+++ services/poke-services.js
@@ -6,11 +6,25 @@ class PokeService{
     
 
     static getPage(index){
-        const url = this.BASE_URL + '/pokemon?limit=' 
+        const url = this.BASE_URL + 'pokemon?limit=' 
                                   + this.PAGE_COUNT
                                   + '&offset='
                                   + (this.PAGE_COUNT * index);
 
-        return fetch(url).then(resp => resp.json())
+        return fetch(url)
+        .then(resp => resp.json())
+        .then(pokemonPage => this.getDetails(pokemonPage.results));
     }
+
+    static getDetails(pokemonNames){
+        const requests = [];
+        for (const pokemon of pokemonNames){
+            const name = pokemon.name;
+            const url = this.BASE_URL + 'pokemon/' + name;
+            const request = fetch(url).then(resp => resp.json())
+            requests.push(request);
+        }
+        return Promise.all(requests)
+    }
+
 }
